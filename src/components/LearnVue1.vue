3e1faf5d2fef992d0cbd1bd116f04c8c113a0103
notsupatchai <supatchair@khontamweb.com>
2024-05-08 13:20:51 +0700

    ทำการ commit ทั้งหมด

---
+++ src/components/LearnVue1.vue
@@ -0,0 +1,50 @@
+<template lang="">
+    <div class="my-profile">
+        <p>My name is{{ name}}</p>
+        <p>i'm age {{age}} year old.</p>
+        <p>My position is {{position}}</p>
+        <p>myFavLang</p>
+        <p v-for="myFavLang in myFavLang" ::key="myFavLang.id"> {{myFavLang.name}}</p>
+        <button @click="count++">click me {{count}}</button>
+        <button @click="name='not123'">change my name</button>
+        <button @click="awesome = !awesome">Toggle</button>
+        <p v-if="awesome">vue is awesome!</p>
+        <p v-else>oh no!</p>
+        
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            name: "notwp",
+            age:26,
+            position: "dev",
+            myFavLang: [
+                {
+                    name: 'java',
+                    id:1
+                },
+                {
+                    name: 'css',
+                    id:2
+                },
+                {
+                    name: 'html',
+                    id:3
+                }
+            ],
+            count: 0,
+            awesome: true
+        }
+    }
+}
+</script>
+<style scoped>
+    .my-profile {
+        background-color: aqua;
+        border-radius: 5px;
+        box-shadow: 3px 3px 10px black;
+        padding: 1rem;
+    }
+</style>
